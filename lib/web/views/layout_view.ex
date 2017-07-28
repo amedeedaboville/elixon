31b44eb9f1835c25e8a2a731ae8b699482519abf
Amedee d'Aboville <amedee.daboville@gmail.com>
2017-07-28 19:55:20 -0400

    implement a javascript_pack_tag and stylesheet_pack_tag so the includes now work with one caveat: In prod mode the pack names have random characters added to each version, so you need to use the webpack manifest, which maps original pack names to their compiled versions. That's my next step is loading the manifest.

--- lib/web/views/layout_view.ex
+++ lib/web/views/layout_view.ex
@@ -1,4 +1,6 @@
 defmodule Elixon.Web.LayoutView do
+  use Phoenix.HTML
+
   use Elixon.Web, :view
   #  def render(_, assigns) do
   #    Map.putassigns.
@@ -7,15 +9,26 @@ defmodule Elixon.Web.LayoutView do
   def title do
     "Awesome New Title!"
   end
-  def stylesheet_pack_tag(param, opts) do
-    "linke to this stylesheet mate:"
+  def stylesheet_pack_tag(pack_name, opts \\ []) do
+    src = "packs/#{pack_name}.js"
+    Phoenix.HTML.Tag.content_tag(:link, "",
+                                 Keyword.put_new(opts, :rel, "stylesheet")
+                                 |> Keyword.put_new(:href, src)
+    )
   end
-  def javascript_pack_tag(filename, opts) do
-    "some Javyscript"
+  def javascript_pack_tag(pack_name, opts \\ []) do
+    #render "javascript_pack_tag.html", pack_name: pack_name
+    src = "packs/#{pack_name}.css"
+    Phoenix.HTML.Tag.content_tag(:script, "",
+                         Keyword.put_new(opts, :src, src)
+                         |> Keyword.put_new(:integrity, "true")
+                         |> Keyword.put_new(:crossorigin, "anonymous")
+                         |> Keyword.put_new(:rel, "preload")
+    )
   end
-  
+
   def csrf_meta_tags do
-    ""
+    Phoenix.HTML.Tag.csrf_meta_tag()
   end
   def header_tags do
     "no header"
